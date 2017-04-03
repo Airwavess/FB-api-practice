018f6c6a8ab3ef4f0a98628b7c5e9ca68c1e4c50
leo <prairie5270@gmail.com>
2017-04-03 13:02:38 +0800

    version 3

--- practice/js/fb-load-part.js
+++ practice/js/fb-load-part.js
@@ -27,24 +27,28 @@ function showData() {
     list.push({
       'likes': likes[i],
       'message': message[i],
-      'created_time': created_time[i]
+      'created_time': created_time[i],
+      'story_url': story_url[i]
     });
 
   list.sort(function(likes_1, likes_2) {
-    return ((likes_1.likes > likes_2.likes) ? -1 : ((likes_1.likes == likes_2.likes) ? 0 : 1));
+    return ((likes_1.likes > likes_2.likes) ? -1 : ((likes_1.likes == likes_2.likes) ? 0 : 1))
   });
 
   for (var k = 0; k < list.length; k++) {
-    likes[k] = list[k].likes;
-    message[k] = list[k].message;
+    likes[k] = list[k].likes
+    message[k] = list[k].message
+    created_time[k] = list[k].created_time
+    story_url[k] = list[k].story_url
   }
-  $(".loader").hide();
+  $(".sk-cube-grid").hide();
   var fb_content = document.getElementById("post-preview")
   for (var i = 0; i < 5; i++) {
     var author = message[i].split("---")[1]
     message[i] = message[i].split("---")[0]
-    fb_content.innerHTML += "<a href='#'>" 
-                          + "<h2 class='post-title' style='font-family: Microsoft JhengHei'>" 
+    fb_content.innerHTML += "<a href="
+                          + story_url[i] 
+                          + "><h2 class='post-title' style='font-family: Microsoft JhengHei'>" 
                           + "主顧榮譽書院劇場          " 
                           + "<img src='img/likes.jpg' width='4%'>" 
                           + likes[i] 
@@ -58,6 +62,8 @@ function showData() {
                           + "</a>" 
                           + created_time[i].split("T")[0] 
                           + "</p>" + "<hr>"
+    fb_content.style.opacity = 1 
+    fb_content.style.top = 0
   }
 
 }
@@ -65,9 +71,9 @@ function showData() {
 var likes = []
 var message = []
 var created_time = []
+var story_url = []
 var getPosts = function(response) {
   var myObj = response
-  console.log(response)
   for (var i = 0; i < myObj.data.length; i++) {
     if (myObj.data[i] != null) {
       if (typeof(myObj.data[i].likes) !== "undefined") {
@@ -75,11 +81,13 @@ var getPosts = function(response) {
       }
       message.push(myObj.data[i].message)
       created_time.push(myObj.data[i].created_time)
+      id = myObj.data[i].id.split("_")[1]
+      story_url.push("https://www.facebook.com/permalink.php?story_fbid="+id+"&id=170659829952078")
     }
   }
 
   if (typeof(response.paging) === "undefined" || typeof(response.paging.next) === "undefined") {
-    console.log("data collected")
+
     showData()
   } else {
     nextPage = response.paging.next
@@ -95,7 +103,7 @@ function getLoginStatus(response) {
     'GET', {
       "access_token": accessToken,
       "limit": 100,
-      "fields": "likes, message, created_time",
+      "fields": "likes, message, created_time, id",
     }, getPosts
   );
 }
--- practice/js/fb-load.js
+++ practice/js/fb-load.js
@@ -26,46 +26,54 @@ function showData() {
     list.push({
       'likes': likes[i],
       'message': message[i],
-      'created_time': created_time[i]
+      'created_time': created_time[i],
+      'story_url' : story_url[i]
     });
 
   list.sort(function(likes_1, likes_2) {
-    return ((likes_1.likes > likes_2.likes) ? -1 : ((likes_1.likes == likes_2.likes) ? 0 : 1));
+    return ((likes_1.likes > likes_2.likes) ? -1 : ((likes_1.likes == likes_2.likes) ? 0 : 1))
   });
 
   for (var k = 0; k < list.length; k++) {
-    likes[k] = list[k].likes;
-    message[k] = list[k].message;
+    likes[k] = list[k].likes
+    message[k] = list[k].message
+    created_time[k] = list[k].created_time
+    story_url[k] = list[k].story_url
   }
-  $(".loader").hide();
+  $(".sk-cube-grid").hide();
   var fb_content = document.getElementById("post-preview")
   for (var i = 0; i < likes.length; i++) {
-    var author = message[i].split("---")[1]
-    message[i] = message[i].split("---")[0]
-    fb_content.innerHTML += "<a href='#'>" 
-                          + "<h2 class='post-title' style='font-family: Microsoft JhengHei'>" 
-                          + "主顧榮譽書院劇場          " 
-                          + "<img src='img/likes.jpg' width='4%'>" 
-                          + likes[i] 
-                          + "</h2>" 
-                          + "<h3 class='post-subtitle' style='font-family: Microsoft JhengHei'>" 
-                          + message[i] 
-                          + "</h3>" 
-                          + "</a>" 
-                          + "<p class='post-meta'>Posted by <a href='#'> "
-                          + (typeof(author) === 'undefined'? "不知道 " : author + "  ")
-                          + "</a>" 
-                          + created_time[i].split("T")[0] 
-                          + "</p>" + "<hr>"
+    if(typeof(message[i]) !== 'undefined') {
+      var author = message[i].split("---")[1]
+      message[i] = message[i].split("---")[0]
+      fb_content.innerHTML += "<a href="
+                            + story_url[i]
+                            + "><h2 class='post-title' style='font-family: Microsoft JhengHei'>" 
+                            + "主顧榮譽書院劇場          " 
+                            + "<img src='img/likes.jpg' width='4%'>" 
+                            + likes[i] 
+                            + "</h2>" 
+                            + "<h3 class='post-subtitle' style='font-family: Microsoft JhengHei'>" 
+                            + message[i] 
+                            + "</h3>" 
+                            + "</a>" 
+                            + "<p class='post-meta'>Posted by <a href='#'> "
+                            + (typeof(author) === 'undefined'? "不知道 " : author + "  ")
+                            + "</a>" 
+                            + created_time[i].split("T")[0] 
+                            + "</p>" + "<hr>"
+      fb_content.style.opacity = 1 
+      fb_content.style.top = 0
+    }
   }
 }
 
 var likes = []
 var message = []
 var created_time = []
+var story_url = []
 var getPosts = function(response) {
   var myObj = response
-  console.log(response)
   for (var i = 0; i < myObj.data.length; i++) {
     if (myObj.data[i] != null) {
       if (typeof(myObj.data[i].likes) !== "undefined") {
@@ -73,11 +81,12 @@ var getPosts = function(response) {
       }
       message.push(myObj.data[i].message)
       created_time.push(myObj.data[i].created_time)
+      id = myObj.data[i].id.split("_")[1]
+      story_url.push("https://www.facebook.com/permalink.php?story_fbid="+id+"&id=170659829952078")
     }
   }
 
   if (typeof(response.paging) === "undefined" || typeof(response.paging.next) === "undefined") {
-    console.log("data collected")
     showData()
   } else {
     nextPage = response.paging.next
